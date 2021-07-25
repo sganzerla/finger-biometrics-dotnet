cde33992d709a03002f9f27c91e579584cbddc7a
Alisson Michel Sganzerla <alisson_sganzerla@yahoo.com.br>
2021-07-25 16:59:12 -0300

    chore: rename project

--- CircuitElectric.API/Program.cs
+++ CircuitElectric.API/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Filmes.API
+namespace CircuitElectric.API
 {
     public class Program
     {
