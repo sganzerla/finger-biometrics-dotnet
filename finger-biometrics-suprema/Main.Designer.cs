fab6d401be6859319e4d1c9358003200958e9e30
Alisson Michel Sganzerla <alisson.sganzerla@gmail.com>
2021-08-06 17:52:54 -0300

    feat: add capture finger

--- finger-biometrics-suprema/Main.Designer.cs
+++ finger-biometrics-suprema/Main.Designer.cs
@@ -37,26 +37,27 @@ namespace finger_biometrics_suprema
             // 
             // buttonCapture
             // 
-            this.buttonCapture.Location = new System.Drawing.Point(562, 380);
+            this.buttonCapture.Location = new System.Drawing.Point(449, 321);
             this.buttonCapture.Name = "buttonCapture";
             this.buttonCapture.Size = new System.Drawing.Size(75, 23);
             this.buttonCapture.TabIndex = 0;
             this.buttonCapture.Text = "Capture";
             this.buttonCapture.UseVisualStyleBackColor = true;
+            this.buttonCapture.Click += new System.EventHandler(this.buttonCapture_Click);
             // 
             // textBoxLogs
             // 
-            this.textBoxLogs.Location = new System.Drawing.Point(72, 72);
+            this.textBoxLogs.Location = new System.Drawing.Point(12, 376);
             this.textBoxLogs.Multiline = true;
             this.textBoxLogs.Name = "textBoxLogs";
             this.textBoxLogs.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
-            this.textBoxLogs.Size = new System.Drawing.Size(288, 289);
+            this.textBoxLogs.Size = new System.Drawing.Size(539, 169);
             this.textBoxLogs.TabIndex = 1;
             // 
             // pictureBoxFinger
             // 
             this.pictureBoxFinger.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBoxFinger.Location = new System.Drawing.Point(427, 72);
+            this.pictureBoxFinger.Location = new System.Drawing.Point(124, 70);
             this.pictureBoxFinger.Name = "pictureBoxFinger";
             this.pictureBoxFinger.Size = new System.Drawing.Size(299, 289);
             this.pictureBoxFinger.TabIndex = 2;
@@ -66,7 +67,7 @@ namespace finger_biometrics_suprema
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(585, 572);
             this.Controls.Add(this.pictureBoxFinger);
             this.Controls.Add(this.textBoxLogs);
             this.Controls.Add(this.buttonCapture);
--- finger-biometrics-suprema/Main.cs
+++ finger-biometrics-suprema/Main.cs
@@ -1,21 +1,143 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Suprema;
+using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace finger_biometrics_suprema
 {
     public partial class Main : Form
     {
+
+        private UFScannerManager _mScannerManager;
+        private UFScanner _scanner;
+        private string _mStrError;
+        private const int TimeoutLedScannerSuprema = 60000;
+        private const int MaxTemplateSize = 1024;
+        private string _base64;
+
         public Main()
         {
             InitializeComponent();
+            LoadScannerSuprema();
         }
 
+        private void LoadScannerSuprema()
+        {
+            _mScannerManager = new UFScannerManager(this);
+            _scanner = new UFScanner();
+            var ufsRes = _mScannerManager.Init();
+            if (ufsRes == UFS_STATUS.OK)
+            {
+                SetLog("UFScanner Init: OK\r\n");
+            }
+            else
+            {
+                UFScanner.GetErrorString(ufsRes, out _mStrError);
+                SetLog($"UFScanner Init: {_mStrError}\r\n");
+                return;
+            }
+
+            _mScannerManager.ScannerEvent += ScannerEvent;
+            var nScannerNumber = _mScannerManager.Scanners.Count;
+            SetLog($"UFScanner GetScannerNumber: {nScannerNumber}\r\n");
+            UpdateScannerListSuprema();
+        }
+
+        private void ScannerEvent(object sender, UFScannerManagerScannerEventArgs e)
+        {
+            UpdateScannerListSuprema();
+        }
+
+
+        private void SetLog(string message)
+        {
+            textBoxLogs.AppendText("     " + DateTime.Now + " - " + message + " \n");
+        }
+
+
+        private void UpdateScannerListSuprema()
+        {
+            var nScannerNumber = _mScannerManager.Scanners.Count;
+
+            if (nScannerNumber == 0)
+            {
+                _scanner = null;
+                SetLog("UFScanner not found GetScannerNumber \r\n");
+            }
+            else
+            {
+                _scanner = _mScannerManager.Scanners[0];
+                SetLog($"Biometria serial: {_scanner.Serial}\r\n");
+                SetLog($"UFScanner GetScannerNumber: {nScannerNumber}\r\n");
+            }
+        }
+
+        private void ClearLogTextMessage()
+        {
+            textBoxLogs.Text = "";
+        }
+
+        private void DrawCapturedImageFingerSuprema()
+        {
+            var ufsRes = _scanner.GetCaptureImageBuffer(out var bitmap, out _);
+            pictureBoxFinger.Image = ufsRes == UFS_STATUS.OK ? bitmap : null;
+        }
+
+        private void ExtractWsqBufferToFileSuprema()
+        {
+            var template = new byte[MaxTemplateSize];
+
+            var ufsRes = _scanner.ExtractEx(MaxTemplateSize, template, out var templateSize, out var enrollQuality);
+
+            if (ufsRes == UFS_STATUS.OK)
+            {
+                SetLog($"Template Size({templateSize}), Quality({enrollQuality})\r\n");
+            }
+            else
+            {
+                UFScanner.GetErrorString(ufsRes, out var mStrError);
+                SetLog("UFScanner ExtractEx: " + mStrError + "\r\n");
+            }
+
+            var filename = $"{Guid.NewGuid()}.wsq";
+            ufsRes = _scanner.SaveCaptureImageBufferToWSQ(filename, 1);
+            if (ufsRes == UFS_STATUS.OK)
+                SetLog($"UFScanner image buffer is read");
+
+            using (var fs = new FileStream(filename, FileMode.Open, FileAccess.Read))
+            {
+                using var reader = new BinaryReader(fs);
+                _base64 = Convert.ToBase64String(reader.ReadBytes((int)fs.Length));
+            }
+
+
+            if (File.Exists(filename))
+                File.Delete(filename);
+        }
+
+
+
+        private void buttonCapture_Click(object sender, EventArgs e)
+        {
+            if (_scanner == null) return;
+
+            _scanner.ClearCaptureImageBuffer();
+            ClearLogTextMessage();
+            _scanner.Timeout = TimeoutLedScannerSuprema;
+
+            var ufsRes = _scanner.CaptureSingleImage();
+
+            if (ufsRes == UFS_STATUS.OK)
+            {
+                SetLog("UFScanner CaptureSingleImage: OK\r\n");
+                DrawCapturedImageFingerSuprema();
+                ExtractWsqBufferToFileSuprema();
+            }
+            else
+            {
+                UFScanner.GetErrorString(ufsRes, out _mStrError);
+                SetLog($"UFScanner CaptureSingleImage: {_mStrError}\r\n");
+            }
+        }
     }
 }
